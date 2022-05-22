5f0fc766c21f223c91da3b0521621c6a402393bf
kei53 <59726332+kei53@users.noreply.github.com>
2022-05-22 15:11:50 +0900

    add-js1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divたぶのこ要素に書く要素を設定
+  div.appendChild(li);
+  console.log(div);
+
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
