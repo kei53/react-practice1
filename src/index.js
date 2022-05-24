b3463640f7c429bf88805a1a542fe38cd3feb3e7
kei53 <59726332+kei53@users.noreply.github.com>
2022-05-24 20:08:40 +0900

    delete_button

--- src/index.js
+++ src/index.js
@@ -11,8 +11,26 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグを未完了リストから削除
+    const deleteTareget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTareget);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグを未完了リストから削除
+    const deleteTareget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTareget);
+  });
+
   //divたぶのこ要素に書く要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   console.log(div);
 
   document.getElementById("incomplete-list").appendChild(div);
